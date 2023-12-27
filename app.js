46ff22d33b7c5edaaf93a14d432f4714ee2a758e
OcherednRra <valentin.polak2004@gmail.com>
2023-12-27 13:34:53 +0200

    Assignment2

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 // app.js
-var app = angular.module('LunchChecker', []);
-app.controller('LunchCheckerController', function($scope) {
-  $scope.lunchMenu = '';
-  $scope.message = '';
+var app = angular.module('NarrowItDownApp', []);
+app.controller('NarrowItDownController', function($scope, $http) {
+  $scope.searchTerm = '';
+  $scope.foundItems = [];
 
-  $scope.checkIfTooMuch = function() {
-    var items = $scope.lunchMenu.split(',');
-    if ($scope.lunchMenu === '') {
-      $scope.message = 'Please enter data first';
-    } else if (items.length <= 3) {
-      $scope.message = 'Enjoy!';
-    } else {
-      $scope.message = 'Too much!';
-    }
+  $scope.narrowItDown = function() {
+    var promise = $http({
+      method: "GET",
+      url: ("https://davids-restaurant.herokuapp.com/menu_items.json")
+    });
+
+    promise.then(function (response) {
+      var items = response.data.menu_items;
+      $scope.foundItems = [];
+
+      for (var i = 0; i < items.length; i++) {
+        if (items[i].description.indexOf($scope.searchTerm) !== -1) {
+          $scope.foundItems.push(items[i]);
+        }
+      }
+    })
+    .catch(function (error) {
+      console.log("Something went terribly wrong.");
+    });
+  };
+
+  $scope.removeItem = function(itemIndex) {
+    $scope.foundItems.splice(itemIndex, 1);
   };
 });
